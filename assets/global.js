21e3ae579428a43a8361fa10993363951daa58b3
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-07-25 19:04:27 +0300

    update style for nav

--- assets/global.js
+++ assets/global.js
@@ -12,20 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".announcement-bar .close-button").addEventListener("click", () => {
     document.querySelector(".announcement-bar").style.display = "none";
   });
-
-  function closeSearch() {
-    const searchSection = document.querySelector(".header__search");
-
-    searchSection.addEventListener("click", (e) => {
-      let detailsSection = searchSection.querySelector("details");
-
-      if (e.target.classList.contains("search-modal__content") && detailsSection.hasAttribute("open")) {
-        detailsSection.removeAttribute("open");
-      }
-    })
-  }
-
-  closeSearch();
 })
 
 
