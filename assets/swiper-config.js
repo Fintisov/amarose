9e5a2f82db4bf8754e233c6ea9b40e5d4a22351c
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-08-10 11:10:49 +0300

    update style for section ingredients

--- assets/swiper-config.js
+++ assets/swiper-config.js
@@ -1,21 +1,33 @@
-const swiper = new Swiper('.ingredients-list', {
+const swiper = new Swiper('.ingredients-list__content', {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1.5,
   spaceBetween: 8,
   allowSlideNext: true,
+  autoHeight: true,
+  centeredSlides: true,
+  watchOverflow: true,
 
   breakpoints: {
-    576: {
-
+    450: {
+      slidesPerView: 2.1,
     },
     767: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+      centeredSlides: false,
+    },
+    991: {
+      spaceBetween: 16,
+      slidesPerView: 2,
+      centeredSlides: false,
+    },
+    1300: {
       spaceBetween: 16,
       slidesPerView: 3,
     }
   },
-  
+
   navigation: {
     nextEl: '.ingredient-btn-next',
-    // prevEl: '.swiper-button-prev',
   },
 });
